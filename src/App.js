debd5e0d11b9454bb39287f1bc04f95ca65e597c
unknown <demab@site.asty-moulin.be>
2021-01-23 17:41:21 +0100

    Ajout de la base de navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './Components/navBar'
 
 function App() {
   return (
     <div className="App">
+        <NavBar />
         <h1>Famille de Mahieu</h1>
     </div>
   );
